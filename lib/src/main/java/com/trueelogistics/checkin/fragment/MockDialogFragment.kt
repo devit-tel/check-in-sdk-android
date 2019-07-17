e3d093e4c361626a38050a4c912782ab94c6dcd9
Ski_True <thanadon_sri@true-e-logistics.com>
2019-07-17 15:47:20 +0700

    test commit

---
+++ lib/src/main/java/com/trueelogistics/checkin/fragment/MockDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.trueelogistics.checkin.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.trueelogistics.checkin.R
+import kotlinx.android.synthetic.main.fragment_mock_dialog.*
+
+class MockDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mock_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        scanAgain.setOnClickListener {
+            dialog.cancel()
+            onResume()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = ViewGroup.LayoutParams.WRAP_CONTENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog.window?.setLayout(width, height)
+    }
+}
