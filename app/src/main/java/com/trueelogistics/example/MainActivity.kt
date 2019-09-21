b8530f1cd014cc724876ed00cb720631fa86a79c
Narongkorn Padcharasomboon <narongkorn_pad@true-e-logisitics.com>
2019-09-21 20:24:34 +0700

    Clear data sdk.

--- app/src/main/java/com/trueelogistics/example/MainActivity.kt
+++ app/src/main/java/com/trueelogistics/example/MainActivity.kt
@@ -1,90 +1,36 @@
 package com.trueelogistics.example
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuItem
-import android.widget.Toast
-import com.trueelogistics.checkin.handler.CheckInTEL
-import com.trueelogistics.checkin.interfaces.CheckInTELCallBack
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        CheckInTEL.userId = "1111111111114"
-        nav_view.setNavigationItemSelectedListener(this)
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frag_main, ScanQrFragment())
-            .commit()
+        bindingData()
     }
 
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+    private fun bindingData() {
+        btnCheckHistory.setOnClickListener { historyCheckIn() }
+        btnScanCheckIn.setOnClickListener { scanCheckIn() }
+        btnNearByCheckIn.setOnClickListener { nearByCheckIn() }
+        btnShakeCheckIn.setOnClickListener { shakeCheckIn() }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.scan_qr -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frag_main, ScanQrFragment())
-                    .commit()
-            }
-            R.id.shake_fine -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frag_main, ShakeFragment())
-                    .commit()
-            }
-            R.id.nearby_fine -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frag_main, NearByFragment())
-                    .commit()
-            }
-            R.id.absence -> {
-                CheckInTEL.checkInTEL?.openMainScanQrCode(this,object : CheckInTELCallBack{
-                    override fun onCheckInSuccess(result: String) {
-                        Toast.makeText(
-                            this@MainActivity, result,
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-
-                    override fun onCheckInFailure(message: String) {
-                        Toast.makeText(
-                            this@MainActivity, message ,
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-
-                    override fun onCancel() {
-                        Toast.makeText(
-                            this@MainActivity, "on cancel",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-
-                })
-            }
-        }
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
+    private fun historyCheckIn() {
     }
 
-    fun actionToolbar() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            drawer_layout.openDrawer(GravityCompat.START)
-        }
+    private fun scanCheckIn() {
+
+    }
+
+    private fun nearByCheckIn() {
+
+    }
+
+    private fun shakeCheckIn() {
+
     }
 }
