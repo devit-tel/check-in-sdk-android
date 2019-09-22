ed4274f28574e92e8986ef97bb0391972cef039f
Narongkorn Padcharasomboon <narongkorn_pad@true-e-logisitics.com>
2019-09-22 09:01:51 +0700

    Clear up.

--- lib/src/main/java/com/trueelogistics/checkin/api/ApiService.kt
+++ lib/src/main/java/com/trueelogistics/checkin/api/ApiService.kt
@@ -1,6 +1,8 @@
 package com.trueelogistics.checkin.api
 
 import com.trueelogistics.checkin.api.service.CheckInService
+import com.trueelogistics.checkin.enums.EnvironmentType
+import com.trueelogistics.checkin.handler.CheckInTEL
 import com.trueelogistics.checkin.model.HeaderModel
 
 
@@ -9,7 +11,10 @@ class ApiService {
     companion object {
 
         fun checkInService(
-            baseUrl: String = "url_config",
+            baseUrl: String = if (CheckInTEL.environmentType == EnvironmentType.Production.value)
+                "http://api.sendit.asia"
+            else
+                "http://api.staging.sendit.asia",
             needHeader: Boolean = true,
             moreHeader: ArrayList<HeaderModel>? = null,
             token: String? = null
